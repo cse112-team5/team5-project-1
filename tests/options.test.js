9117d118a319a45690bc4cec9b2a2de8b3298c87
kra008 <kra008@ucsd.edu>
2020-05-11 16:39:36 -0700

    Committing from base directory to include all changes

--- tests/options.test.js
+++ tests/options.test.js
@@ -2,9 +2,10 @@ const test = document.getElementById("test4")
 
 function testUpdateDomainProductive() {
     console.log("Testing updateDomainProductive");
-    if (test.innerHTML != updateDomainProductive("", true)) {
+    if (updateDomainProductive("", true) != -1) {
         test.innerHTML = "FAILED: didn't check for"
     }
+    test.innerHTML = "Passed: properly returns 0 on sucess";
     //updateDomainProductive("unproductive.com", false);
 }
 testUpdateDomainProductive();
