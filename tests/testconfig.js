02361061e712b67543fed1417a6ba71e94fc9aab
kra008 <kra008@ucsd.edu>
2020-05-18 16:19:31 -0700

    added testconfig file

---
+++ tests/testconfig.js
@@ -0,0 +1,16 @@
+/*
+ * Config information for testing database
+ */
+
+var config = {
+    apiKey: "AIzaSyCOhTt25qhJtQyWSEUFCU3s_ZE9EC3EiGs",
+    authDomain: "cse112-sp20.firebaseapp.com",
+    databaseURL: "https://cse112-sp20.firebaseio.com",
+    projectId: "cse112-sp20",
+    storageBucket: "cse112-sp20.appspot.com",
+    messagingSenderId: "861300546651",
+    appId: "1:861300546651:web:93eb90114a9f3e6df1737e"
+};
+  
+firebase.initializeApp(config);
+  
